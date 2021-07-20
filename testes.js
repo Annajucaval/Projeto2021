86214484784c1d54c57e969dcb4d76ce29c1c30b
Naju caval <annajuliacaval@gmail.com>
2021-07-20 14:18:39 -0300

    teste de java natela de login

--- testes.js
+++ testes.js
@@ -1,1 +1,39 @@
-console.log("Naju arrumando o site de testes");
+
+function pulaLinha() {
+
+    document.write("<br>");
+}
+
+function mostra(frase) {
+
+    document.write(frase);
+    pulaLinha();
+}
+
+var Imputemail = "alura";
+var Imputpassaword = "alura321";
+
+var tentativas = 1;
+
+while (tentativas <= 3) {
+
+     loginInformado = prompt("Informe seu login");
+     senhaInformada = prompt("Informe sua senha");
+
+
+    if( loginCadastrado == loginInformado && senhaCadastrada == senhaInformada ) {
+
+        mostra("Bem-vindo ao sistema " + loginInformado);
+
+
+        break; 
+
+    } else {
+
+        alert("Login inválido. Tente novamente");
+        tentativas++;
+
+    }
+
+
+}
